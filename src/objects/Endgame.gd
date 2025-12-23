50ce668a672b75f8a70588105f91ab8b98164bc5
prokenya <prokenya3d@gmail.com>
2025-12-23 14:36:38 +0200

    wire breakage mechanics

--- src/objects/Endgame.gd
+++ src/objects/Endgame.gd
@@ -1,12 +1,13 @@
-extends SpaceObject
 class_name Endgame
+extends SpaceObject
 
-@export var target_strength:int = 1
+@export var target_strength: int = 1
 
 
 func _ready() -> void:
 	super()
-	object_connected.connect(func(obj):check_condition())
+	object_connected.connect(func(obj): check_condition())
+
 
 func check_condition():
 	if signal_strength == target_strength:
